c884946364226038334f215906344f3e2debedd8
devanshi20 <devanshimadhani20@gmail.com>
2018-01-24 21:07:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/admin/androideatitserver/Common/Common.java
@@ -0,0 +1,12 @@
+package com.example.admin.androideatitserver.Common;
+
+import com.example.admin.androideatitserver.Model.User;
+
+/**
+ * Created by Admin on 24-Jan-18.
+ */
+
+public class Common {
+
+    public static User currentUser;
+}
